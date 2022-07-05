6c0f6a4bc8c10da995dcca9b5052b1598aecd080
isaac mariga <isaacwangombe5@gmail.com>
2022-07-05 16:35:06 +0300

    Making front end edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { MedicinesComponent } from './medicines/medicines.component';
-
+import { MainComponent } from './main/main.component';
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'medicines', component: MedicinesComponent },
+  { path: 'main', component: MainComponent },
 ];
 
 @NgModule({
--- src/app/medicines/medicines.component.ts
+++ src/app/medicines/medicines.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+export class Medicines {
+  constructor(
+    public name: string,
+    public price: number,
+    public description: string,
+    public picture: string
+  ) {}
+}
+
+export class Diseases {
+  constructor(public name: string) {}
+}
 
 @Component({
   selector: 'app-medicines',
   templateUrl: './medicines.component.html',
-  styleUrls: ['./medicines.component.css']
+  styleUrls: ['./medicines.component.css'],
 })
 export class MedicinesComponent implements OnInit {
+  medicines: Medicines[];
+  diseases: Diseases[];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.getMedicines();
+    this.getDiseases();
   }
 
+  selectedDisease: string = 'Leukimia';
+  @Output()
+  selectedChanged: EventEmitter<string> = new EventEmitter<string>();
+  onSelectedChanged() {
+    // console.log(this.selectedDisease);
+  }
+
+  getMedicines() {
+    this.selectedChanged.emit(this.selectedDisease);
+    console.log('test');
+
+    this.httpClient
+      .get<any>(
+        'https://medistore-apis.herokuapp.com/api/medicine/' +
+          this.selectedDisease
+      )
+      .subscribe((response) => {
+        console.log('test');
+        this.medicines = response;
+      });
+  }
+  getDiseases() {
+    this.httpClient
+      .get<any>('https://medistore-apis.herokuapp.com/api/disease/')
+      .subscribe((response) => {
+        this.diseases = response;
+        // console.log(response);
+      });
+  }
 }
